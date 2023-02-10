7d801a503b181171aba253b64f87a20aafc459c6
vedantiSharma <93864090+vedantiSharma@users.noreply.github.com>
2023-02-10 23:09:58 +0530

    adding images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import vg from "../assets/2.jpg";
+import vg from './1.jpg';
 
 const Header = () => {
   return (
     <>
-    <div className="home">
-      <main>
-        <h1>TechyStart.</h1>
-        <p>Solution to all your problems</p>
-      </main>
-    </div>
-     
-
-     <div className = "home2">
-      <img src= {vg}  alt="Graphics"/>
-      <div>
-        <p>
-          qqqqqqqqqqqqqqqqqpppppppp ppppppppeeeeeee pppppppppfffff fjfnhbfhvb vsnihvkhnvv uvuewinhwvnjvns vnn hnuwbvbiuvbuijkn jviwnvuwjnebjb  vbviwlvnhijnw
-        </p>
+      <div className="home">
+        <main>
+          <h1>TechyStart.</h1>
+          <p>Solution to all your problems</p>
+        </main>
       </div>
 
-     </div>
-
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>
+            qqqqqqqqqqqqqqqqqpppppppp ppppppppeeeeeee pppppppppfffff fjfnhbfhvb
+            vsnihvkhnvv uvuewinhwvnjvns vnn hnuwbvbiuvbuijkn jviwnvuwjnebjb
+            vbviwlvnhijnw
+          </p>
+        </div>
+      </div>
     </>
   );
 };
