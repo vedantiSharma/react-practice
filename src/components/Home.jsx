0c257c649ff733c65af363b7f201b8e59d8a343e
vedantiSharma <93864090+vedantiSharma@users.noreply.github.com>
2023-02-10 22:31:00 +0530

    1 binaries uploaded

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import vg from "../assets/";
 const Header = () => {
   return (
+    <>
     <div className="home">
       <main>
         <h1>TechyStart.</h1>
         <p>Solution to all your problems</p>
       </main>
     </div>
+     
+
+     <div className = "home2">
+      <img src={vg}  alt="Graphics"/>
+      <div>
+        <p>
+          qqqqqqqqqqqqqqqqqpppppppp ppppppppeeeeeee pppppppppfffff fjfnhbfhvb vsnihvkhnvv uvuewinhwvnjvns vnn hnuwbvbiuvbuijkn jviwnvuwjnebjb  vbviwlvnhijnw
+        </p>
+      </div>
+
+     </div>
+
+    </>
   );
 };
 
